aa9d92f83ff4f84bee6b2d8bb525678a4632b4bb
Osman Ramadan <oior2@data.cambridgeauv.co.uk>
2015-12-31 02:33:53 +0000

    first commit

---
+++ src/test/java/com/library/management/system/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,18 @@
+package com.library.management.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LibraryManagementSystemApplication.class)
+@WebAppConfiguration
+public class LibraryManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
